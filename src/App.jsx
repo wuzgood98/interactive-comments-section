a881a836dfa9e2069d1e228660afa5d7e7f374fa
Gideon Addo <gideonaddoofori@gmail.com>
2022-11-09 21:31:59 +0000

    Contain elements with landmark

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,13 @@ function App() {
   }, [refContainer]);
 
   return (
-    <div className="bg-lightGray w-full min-h-screen py-5 px-4 font-rubik flex flex-col gap-4">
+    <main className="bg-lightGray w-full min-h-screen py-5 px-4 font-rubik flex flex-col gap-4">
       <Modal />
-      <main ref={refContainer} className="w-full sm:max-w-[45rem] sm:min-h-[40rem] sm:m-auto space-y-4 sm:space-y-5">
+      <section ref={refContainer} className="w-full sm:max-w-[45rem] sm:min-h-[40rem] sm:m-auto space-y-4 sm:space-y-5">
         <Comments />
         <Editor />
-      </main>
-    </div>
+      </section>
+    </main>
   )
 }
 
