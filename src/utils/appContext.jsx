0d73655d2a6248e114d49cc5ac2981649c199c5c
Gideon Addo <gideonaddoofori@gmail.com>
2022-12-17 16:23:41 +0000

    Update data.json file path

--- src/utils/appContext.jsx
+++ src/utils/appContext.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import commentsData from '../../data'
+import commentsData from '../../data.json'
 
 import useLocalStorage from "./useLocalStorage";
 
